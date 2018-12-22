bf3c2281374c97bb4e821bf24959b7f6f052cc9e
rafael <rafael.nunu@hotmail.com>
2018-12-22 00:16:54 -0200

    template base

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,26 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'deion', name: 'deion', content: 'Nuxt.js project' }
+    ],
+    : [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
+      { src: 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js'},
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'},
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js'},
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.proto.min.js'},
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  /**
+   * Global CSS
+   */
+  css: [
+  ],
+  
   /*
   ** Customize the progress bar color
   */
@@ -21,6 +35,9 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    extractCSS: {
+      allChunks: true
+    },
     /*
     ** Run ESLint on save
     */
