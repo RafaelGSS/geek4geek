e8800329ce7d36409ebbb4b6593200c89ed08a6a
rafaelgss <rafael.nunu@hotmail.com>
2018-12-23 18:06:10 -0200

    added test foldeR

---
+++ pages/tests/header-top.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderTop />
+</template>
+
+<script>
+import HeaderTop from '~/components/HeaderTop.vue'
+
+export default {
+  components: {
+    HeaderTop
+  }
+}
+</script>
+
+<style>
+</style>
+
