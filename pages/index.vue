d78e2ecc69f74242fe2e629b3c26d374254586d3
rafaelgss <rafael.nunu@hotmail.com>
2018-12-26 18:58:59 -0200

    added footer on tests folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
+<div>
+  <HeaderTop />
+  <FooterBottom />
+</div>
 </template>
 
 <script>
 
+import HeaderTop from '~/components/HeaderTop.vue'
+import FooterBottom from '~/components/FooterBottom.vue'
+
 export default {
- 
+  components: {
+    HeaderTop, FooterBottom
+  }
 }
 </script>
 
