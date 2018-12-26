188c18cda248a14e4b1e5fe5cf15bb4af5a4c94e
Yuri Silva <nogueiranetoy@gmail.com>
2018-12-26 12:25:47 -0200

    criação do footer bottom

---
+++ pages/example-footerbottom.vue
@@ -0,0 +1,19 @@
+<template>
+    <Footerbottom />
+</template>
+
+<style>
+
+</style>
+
+<script>
+import Footerbottom from '~/components/Footerbottom.vue'
+
+export default {
+    components: {
+        Footerbottom
+    }
+}
+</script>
+
+
