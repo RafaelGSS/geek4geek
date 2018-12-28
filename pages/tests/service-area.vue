d6797564eae04357876bb11bd9485fe018a86c18
Yuri Silva <nogueiranetoy@gmail.com>
2018-12-28 14:44:58 -0200

    create component service-area

---
+++ pages/tests/service-area.vue
@@ -0,0 +1,18 @@
+<template>
+    <ServiceArea />
+</template>
+
+<style>
+
+</style>
+
+<script>
+import ServiceArea from '~/components/ServiceArea.vue'
+
+export default {
+    components: {
+        ServiceArea
+    }
+}
+</script>
+
