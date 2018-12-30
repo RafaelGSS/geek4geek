c64a06ed307d4956830a9defcf804b8411ac052d
rafaelgss <rafael.nunu@hotmail.com>
2018-12-29 22:20:14 -0200

    best-selling and banner area added

---
+++ components/BestSellingArea.vue
@@ -0,0 +1,505 @@
+<template>
+  <div class="best-selling-area best-selling-area-toy pt-60 best-selling-small">
+    <div class="container">
+      <div class="top-bar section-border mb-35">
+        <div class="section-title-wrapper section-bg-white">
+          <h4 class="section-title">Produtos mais vendidos</h4>
+        </div>
+        <div class="product-tab-list-2 nav section-bg-white">
+          <a class="active" href="#home4" data-toggle="tab">
+            <h4>Rolamento de bloco</h4>
+          </a>
+          <a href="#home5" data-toggle="tab">
+            <h4>Rolamento do bloco de descanso</h4>
+          </a>
+        </div>
+      </div>
+      <div class="tab-content jump">
+        <div class="tab-pane active" id="home4">
+          <div class="best-selling-active-2 nav-style nav-style-red">
+            <div class="best-selling-bundle-2">
+              <div class="row">
+                <div class="col-xl-8 col-lg-12 col-md-12">
+                  <div
+                    class="best-selling-pro-wrapper mb-20 best-selling-big-img devita-product-red"
+                  >
+                    <div class="product-img best-selling-img">
+                      <a href="product-details.html">
+                        <img alt src="/img/product/best-selling-59.jpg">
+                      </a>
+                      <span>-10%</span>
+                    </div>
+                    <div class="product-content best-pro-content best-selling-style-2">
+                      <span>Acessórios</span>
+                      <h4>
+                        <a
+                          href="product-details.html"
+                        >Urso de pelucia, Guarda de Urso (25cm). 1 ... Cor Vermelha ... Adoravelmente inteligente.</a>
+                      </h4>
+                      <div class="product-price-wrapper">
+                        <span>$160.00</span>
+                        <span class="product-price-old">262.00</span>
+                      </div>
+                      <div class="product-list">
+                        <ul>
+                          <li>Som Baixo e estéreo</li>
+                          <li>Comfortable Wearing</li>
+                          <li>Fit Universal Devices & Tangle-free Braided Cable</li>
+                          <li>Lightweight Portable Design</li>
+                        </ul>
+                      </div>
+                      <div class="best-pro-action-2">
+                        <a
+                          class="action-cart"
+                          title="Add cart"
+                          href="#"
+                        >Add cart</a>
+                        <a class="same-action" title="Wishlist" href="#">
+                          <i class="fa fa-heart-o"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-4 col-lg-12 col-md-12">
+                  <div class="row">
+                    <div class="col-xl-12 col-lg-6 col-md-6">
+                      <div class="best-selling-pro-wrapper mb-20 devita-product-red">
+                        <div class="product-img">
+                          <a href="product-details.html">
+                            <img alt src="/img/product/best-selling-60.jpg">
+                          </a>
+                          <span>-10%</span>
+                        </div>
+                        <div class="product-content best-pro-content">
+                          <span>Watch, Accessories</span>
+                          <h4>
+                            <a href="product-details.html">Pikachu Skull Plush</a>
+                          </h4>
+                          <div class="product-price-wrapper">
+                            <span>$180.00</span>
+                            <span class="product-price-old">$200.00</span>
+                          </div>
+                          <div class="product-action best-pro-action">
+                            <a class="same-action" title="Wishlist" href="#">
+                              <i class="fa fa-heart-o"></i>
+                            </a>
+                            <a
+                              class="action-cart"
+                              title="Add cart"
+                              href="#"
+                            >Add cart</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-12 col-lg-6 col-md-6">
+                      <div class="best-selling-pro-wrapper mb-20 devita-product-red">
+                        <div class="product-img">
+                          <a href="product-details.html">
+                            <img alt src="/img/product/best-selling-61.jpg">
+                          </a>
+                        </div>
+                        <div class="product-content best-pro-content">
+                          <span>Phone, Accessories</span>
+                          <h4>
+                            <a href="product-details.html">Disney Baby Mickey</a>
+                          </h4>
+                          <div class="product-price-wrapper">
+                            <span>$260.00</span>
+                          </div>
+                          <div class="product-action best-pro-action">
+                            <a class="same-action" title="Wishlist" href="#">
+                              <i class="fa fa-heart-o"></i>
+                            </a>
+                            <a
+                              class="action-cart"
+                              title="Add cart"
+                              href="#"
+                            >Add cart</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="tab-pane" id="home5">
+          <div class="best-selling-active-2 nav-style nav-style-red">
+            <div class="best-selling-bundle-2">
+              <div class="row">
+                <div class="col-xl-8 col-lg-12 col-md-12">
+                  <div
+                    class="best-selling-pro-wrapper mb-20 best-selling-big-img devita-product-red"
+                  >
+                    <div class="product-img best-selling-img">
+                      <a href="product-details.html">
+                        <img alt src="/img/product/best-selling-62.jpg">
+                      </a>
+                      <span>-10%</span>
+                    </div>
+                    <div class="product-content best-pro-content best-selling-style-2">
+                      <span>Tab, Accessories</span>
+                      <h4>
+                        <a
+                          href="product-details.html"
+                        >Buzz lightyear costume inspiration: white long sleeve with green tank top.</a>
+                      </h4>
+                      <div class="product-price-wrapper">
+                        <span>$260.00</span>
+                        <span class="product-price-old">$162.00</span>
+                      </div>
+                      <div class="product-list">
+                        <ul>
+                          <li>Bass and Stereo Sound</li>
+                          <li>Comfortable Wearing</li>
+                          <li>Fit Universal Devices & Tangle-free Braided Cable</li>
+                          <li>Lightweight Portable Design</li>
+                        </ul>
+                      </div>
+                      <div class="best-pro-action-2">
+                        <a
+                          class="action-cart"
+                          title="Add cart"
+                          href="#"
+                        >Add cart</a>
+                        <a class="same-action" title="Wishlist" href="#">
+                          <i class="fa fa-heart-o"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-4 col-lg-12 col-md-12">
+                  <div class="row">
+                    <div class="col-xl-12 col-lg-6 col-md-6">
+                      <div class="best-selling-pro-wrapper mb-20 devita-product-red">
+                        <div class="product-img">
+                          <a href="product-details.html">
+                            <img alt src="/img/product/best-selling-64.jpg">
+                          </a>
+                          <span>-10%</span>
+                        </div>
+                        <div class="product-content best-pro-content">
+                          <span>Phone, Accessories</span>
+                          <h4>
+                            <a href="product-details.html">Home Wall Sticker</a>
+                          </h4>
+                          <div class="product-price-wrapper">
+                            <span>$260.00</span>
+                            <span class="product-price-old">$162.00</span>
+                          </div>
+                          <div class="product-action best-pro-action">
+                            <a class="same-action" title="Wishlist" href="#">
+                              <i class="fa fa-heart-o"></i>
+                            </a>
+                            <a
+                              class="action-cart"
+                              title="Add cart"
+                              href="#"
+                            >Add cart</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-12 col-lg-6 col-md-6">
+                      <div class="best-selling-pro-wrapper mb-20 devita-product-red">
+                        <div class="product-img">
+                          <a href="product-details.html">
+                            <img alt src="/img/product/best-selling-65.jpg">
+                          </a>
+                        </div>
+                        <div class="product-content best-pro-content">
+                          <span>Remote, Accessories</span>
+                          <h4>
+                            <a href="product-details.html">Babies Bumpkin Dog</a>
+                          </h4>
+                          <div class="product-price-wrapper">
+                            <span>$260.00</span>
+                            <span class="product-price-old">$162.00</span>
+                          </div>
+                          <div class="product-action best-pro-action">
+                            <a class="same-action" title="Wishlist" href="#">
+                              <i class="fa fa-heart-o"></i>
+                            </a>
+                            <a
+                              class="action-cart"
+                              title="Add cart"
+                              href="#"
+                            >Add cart</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.product-img img {
+    width: 100%;
+}
+.product-content {
+    padding: 3px 18px 0;
+}
+.product-img > span {
+    background-color: #0363cd;
+    border-radius: 3px;
+    color: #fff;
+    left: 15px;
+    line-height: 1;
+    padding: 6px 14px 5px;
+    position: absolute;
+    top: 15px;
+    z-index: 99;
+}
+.product-content > span {
+    color: #555;
+    font-size: 13px;
+}
+.product-content > h4 {
+    font-size: 14px;
+    line-height: 18px;
+    margin: 8px 0 13px;
+}
+.product-content > h4 a:hover {
+    color: #0363cd;
+}
+.product-price-wrapper > span {
+    color: #242424;
+    margin: 0 2px;
+}
+.product-price-wrapper > span.product-price-old {
+    text-decoration: line-through;
+}
+
+.product-action {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 -10px;
+}
+.product-action > a {
+    margin: 0 3px;
+}
+.product-action > a i {
+    text-align: center;
+}
+.product-action > a.same-action {
+    background-color: #eef0f1;
+    border-radius: 3px;
+    color: #242424;
+    font-size: 16px;
+    height: 35px;
+    line-height: 35px;
+    text-align: center;
+    width: 35px;
+}
+.product-action > a.action-cart {
+    background-color: #eef0f1;
+    border-radius: 3px;
+    color: #242424;
+    display: inline-block;
+    font-size: 12px;
+    font-weight: 500;
+    padding: 9px 20px 8px;
+    text-transform: uppercase;
+}
+.product-action > a.same-action:hover,
+.product-action > a.action-cart:hover {
+    background-color: #0363cd;
+    color: #fff;
+}
+
+.product-price-wrapper {
+    display: block;
+    transition: all 0.3s ease 0s;
+}
+
+.tab-content.jump > .tab-pane {
+    display: block;
+    height: 0;
+    opacity: 0;
+    overflow: hidden;
+}
+.tab-content.jump > .tab-pane.active {
+    display: block;
+    height: auto;
+    opacity: 1;
+    overflow: visible;
+}
+.section-border {
+    margin-right: 94px;
+    overflow: hidden;
+    position: relative;
+}
+.section-border::before {
+    background-color: #eeeeee;
+    content: "";
+    height: 1px;
+    left: 0;
+    position: absolute;
+    top: 10px;
+    transition: all 0.4s ease 0s;
+    width: 100%;
+    z-index: 1;
+}
+.section-title-wrapper {
+    display: inline-block;
+    float: left;
+}
+.section-bg-white {
+    background-color: #fff;
+    position: relative;
+    z-index: 9;
+}
+.product-tab-list-2 {
+    display: inline-block;
+    float: right;
+    padding: 0 0px 0 20px;
+}
+.section-title {
+    font-size: 20px;
+    font-weight: 500;
+    margin: 0;
+    padding-right: 30px;
+    display: inline-block;
+}
+.product-tab-list-2 a {
+    display: inline-block;
+    margin-left: 15px;
+}
+.product-tab-list-2 a:first-child {
+    margin-left: 0px;
+}
+.product-tab-list-2 a h4 {
+    color: #777;
+    font-size: 14px;
+    transition: all .3s ease 0s;
+}
+.product-tab-list-2 a.active h4 {
+    color: #242424;
+}
+.best-selling-pro-wrapper {
+    background-color: #fff;
+    border-radius: 3px;
+    display: flex;
+    transition: all .3s ease 0s;
+}
+.best-selling-pro-wrapper:hover {
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+}
+.best-selling-bundle {
+    margin-top: 15px;
+}
+.best-selling-pro-wrapper .product-img > span {
+    left: 20px;
+    top: 20px;
+}
+.best-selling-pro-wrapper .product-img {
+    flex: 0 0 180px;
+    position: relative;
+}
+.product-content.best-pro-content {
+    padding: 13px 13px 5px 10px;
+}
+.product-action.best-pro-action > a.action-cart {
+    padding: 8px 20px 9px;
+}
+.product-action.best-pro-action {
+    margin: 16px 0 0;
+    opacity: 0;
+    transform: translateY(-30%);
+    transition: all 0.3s ease 0s;
+    justify-content: left;
+}
+.best-selling-pro-wrapper:hover .product-action.best-pro-action {
+    opacity: 1;
+    transform: translateY(0%);
+}
+.best-selling-active div[class^="col-"] {
+    flex: 0 0 100%;
+    max-width: 100%;
+}
+
+.best-selling-pro-wrapper .product-img.best-selling-img {
+    flex: 0 0 380px;
+}
+.product-list ul li {
+    color: #242424;
+    list-style: outside none none;
+    margin: 0 0 5px;
+    padding: 0 0 0 15px;
+    position: relative;
+}
+.product-list ul li::before {
+    background-color: #888;
+    border-radius: 50px;
+    content: "";
+    height: 5px;
+    left: 0;
+    position: absolute;
+    top: 7px;
+    width: 5px;
+}
+.product-list ul li:last-child {
+    margin: 0 0 0px;
+}
+.product-list {
+    margin: 28px 0 31px;
+}
+.best-pro-action-2 a {
+    background-color: #eef0f1;
+    border-radius: 3px;
+    color: #242424;
+    display: inline-block;
+    font-size: 12px;
+    font-weight: 500;
+    margin: 0 7px 0 0;
+    text-transform: uppercase;
+}
+.best-pro-action-2 a:hover {
+    background-color: #0363cd;
+    color: #fff;
+}
+.best-pro-action-2 {
+    display: flex;
+    flex-wrap: wrap;
+}
+.best-pro-action-2 a.action-cart {
+    padding: 8px 43px 9px;
+}
+.best-pro-action-2 a.same-action {
+    border-radius: 3px;
+    font-size: 16px;
+    height: 35px;
+    line-height: 35px;
+    text-align: center;
+    width: 35px;
+}
+.product-content.best-pro-content.best-selling-style-2 {
+    padding: 40px 13px 5px 10px;
+}
+.best-selling-bundle-2 .row {
+    margin-left: -10px;
+    margin-right: -10px;
+}
+.best-selling-bundle-2 .row div[class^="col-"] {
+    padding-left: 10px;
+    padding-right: 10px;
+}
+.best-selling-active-2.nav-style.owl-carousel .owl-nav div {
+    top: -67px;
+}
+</style>
