aac7f74ca058d092eda9722d1f02f5cf1df0a6e4
RafaelGSS <rafael.nunu@hotmail.com>
2019-01-25 00:59:24 -0200

    fix responsive shop page and build components shoppage

--- components/ProductArea.vue
+++ components/ProductArea.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="product-area pt-35 pb-30">
     <div class="container">
-      <div class="product-tab-list product-tab-list-red mb-30 nav" role="tablist">
+      <div class="product-tab-list mb-30 nav" role="tablist">
         <a href="#novas-chegadas" data-toggle="tab" class="btn-tab active" @click="showNews">
-          <!-- <a class="active" href="#home1" data-toggle="tab"> -->
           <h4>Novas Chegadas</h4>
         </a>
         <a href="#destaques" data-toggle="tab" class="btn-tab" @click="showHots">
-          <!-- <a href="#home2" data-toggle="tab"> -->
           <h4>Destaques</h4>
         </a>
       </div>
@@ -24,116 +22,6 @@
           :isActive="false"
           :itemsPerRow="5"
         />
-        <!-- <div class="tab-pane active" id="home1">
-          <div class="custom-row">
-            <div class="custom-col-5 mb-30" v-for="product_new in products_new" :key="product_new.id">
-              <div class="geek4-product-2 mrg-inherit geek4-product-red">
-                <div class="product-img">
-                  <div class="product-img-slider">
-                    <a href="#">
-                      <img src="/img/product/pro-toy-1.jpg" alt>
-                    </a>
-                  </div>
-                  <span v-if="product_new.promo">-{{ product_new.promotion.percentage }}%</span>
-                </div>
-                <div class="list-col">
-                  <div class="gridview">
-                    <div class="product-content text-center">
-                      <span>{{ product_new.category_name }}</span>
-                      <h4>
-                        <a href="#">{{ product_new.name }}</a>
-                      </h4>
-                      <div class="product-price-wrapper">
-                        <span>R$ {{ product_new.price }}</span>
-                        <span class="product-price-old" v-if="product_new.promo">R$ {{ product_new.promotion.old_price }}</span>
-                      </div>
-                    </div>
-                    <div class="product-action-wrapper-2 text-center">
-                      <div class="product-rating">
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline"></i>
-                      </div>
-                      <p> {{ product_new.display_description }}</p>
-                      <div class="product-action">
-                        <a class="same-action" title="Wishlist" href="#">
-                          <i class="fa fa-heart-o"></i>
-                        </a>
-                        <a
-                          class="action-cart"
-                          href="#"
-                          title="ADICIONAR AO CARRINHO"
-                         @click="addCart({
-                              id: product_new.id,
-                              display_name: product_new.name,
-                              quantity: 1,
-                              value: product_new.price})"
-                        >+ CARRINHO</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="tab-pane" id="home2">
-          <div class="custom-row">
-            <div class="custom-col-5 mb-30" v-for="product_hot in products_hot" :key="product_hot.id">
-              <div class="geek4-product-2 mrg-inherit geek4-product-red">
-                <div class="product-img">
-                  <div class="product-img-slider">
-                    <a href="#">
-                      <img src="/img/product/pro-toy-1.jpg" alt>
-                    </a>
-                  </div>
-                  <span v-if="product_hot.promo">{{ product_hot.promotion.percentage }}</span>
-                </div>
-                <div class="list-col">
-                  <div class="gridview">
-                    <div class="product-content text-center">
-                      <span>{{ product_hot.category_name }}</span>
-                      <h4>
-                        <a href="#">{{ product_hot.name }}</a>
-                      </h4>
-                      <div class="product-price-wrapper">
-                        <span>R$ {{ product_hot.price }}</span>
-                        <span class="product-price-old" v-if="product_hot.promo">R$ {{ product_hot.promotion.old_price }}</span>
-                      </div>
-                    </div>
-                    <div class="product-action-wrapper-2 text-center">
-                      <div class="product-rating">
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline theme-star"></i>
-                        <i class="ion-android-star-outline"></i>
-                      </div>
-                      <p> {{ product_hot.display_description }}</p>
-                      <div class="product-action">
-                        <a class="same-action" title="Wishlist" href="#">
-                          <i class="fa fa-heart-o"></i>
-                        </a>
-                        <a
-                          class="action-cart"
-                          title="ADICIONAR AO CARRINHO"
-                          href="#"
-                          @click="addCart({
-                              id: product_hot.id,
-                              display_name: product_hot.name,
-                              quantity: 1,
-                              value: product_hot.price})"
-                        >+ CARRINHO</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>-->
       </div>
     </div>
   </div>
@@ -147,19 +35,19 @@
   position: relative;
 }
 
-.tab-pane {
+/* .tab-pane {
   display: block;
   height: 0;
   opacity: 0;
   overflow: hidden;
 }
-/* .tab-content.jump >  */
 .tab-pane.active {
   display: block;
   height: auto;
   opacity: 1;
   overflow: visible;
-}
+} */
+
 .product-tab-list,
 .product-tab-list-3 {
   display: flex;
---
+++ components/SidebarFilter/SidebarFilter.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="shop-sidebar-wrapper gray-bg-7 mrg-top-md mrg-top-sm">
+    <div class="shop-widget">
+      <h4 class="shop-sidebar-title">Shop By Categories</h4>
+      <div class="shop-category">
+        <ul id="faq">
+          <li>
+            <a data-toggle="collapse" data-parent="#faq" href="#shop-category-1">
+              Laptop &amp; Computer
+              <i class="ion-ios-arrow-down"></i>
+            </a>
+            <ul id="shop-category-1" class="panel-collapse collapse show">
+              <li>
+                <a href="#">Accessories</a>
+              </li>
+              <li>
+                <a href="#">Bags &amp; Cases</a>
+              </li>
+              <li>
+                <a href="#">Binoculars &amp; Scopes</a>
+              </li>
+              <li>
+                <a href="#">Digital Cameras</a>
+              </li>
+              <li>
+                <a href="#">Film Photography</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Accessories</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="shop-price-filter mt-40 shop-sidebar-border pt-35">
+      <h4 class="shop-sidebar-title">Price Filter</h4>
+      <div class="price_filter mt-25">
+        <span>Range: $100.00 - 1.300.00</span>
+        <div
+          id="slider-range"
+          class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"
+        >
+          <div
+            class="ui-slider-range ui-widget-header ui-corner-all"
+            style="left: 25%; width: 41.6667%;"
+          ></div>
+          <span
+            class="ui-slider-handle ui-state-default ui-corner-all"
+            tabindex="0"
+            style="left: 25%;"
+          ></span>
+          <span
+            class="ui-slider-handle ui-state-default ui-corner-all"
+            tabindex="0"
+            style="left: 66.6667%;"
+          ></span>
+        </div>
+        <div class="price_slider_amount">
+          <div class="label-input">
+            <input type="text" id="amount" name="price" placeholder="Add Your Price">
+          </div>
+          <button type="button">Filter</button>
+        </div>
+      </div>
+    </div>
+    <ShopWidget :title="'Cores'" :type="'checkbox'" />
+    <ShopWidget :title="'Tags'" :type="'tags'" />
+  </div>
+</template>
+
+<style scoped>
+.shop-sidebar-wrapper {
+    border: 1px solid #efefef;
+    padding: 17px 18px 29px;
+}
+
+.shop-category > ul {
+    border-top: 1px solid #ebebeb;
+    margin: 18px 0 0;
+    padding: 27px 0 0;
+}
+.shop-category ul > li {
+    display: block;
+    list-style: outside none none;
+    padding: 0 0 13px;
+}
+.shop-category ul > li:last-child {
+    padding: 0 0 0px;
+}
+.shop-category ul > li ul li:last-child {
+    padding: 7px 0 0 15px;
+}
+.shop-category ul > li a {
+    color: #242424;
+    display: block;
+}
+.shop-category ul li:hover > a {
+    color: #047afe;
+}
+.shop-category ul li:hover > a i {
+    color: #047afe;
+}
+.shop-category ul > li a i {
+    float: right;
+    color: #7a7a7a;
+    font-size: 14px;
+}
+.shop-category ul > li > ul li {
+    padding: 7px 0 0 15px;
+}
+.shop-category ul > li ul li a {
+    color: #666;
+}
+.price_filter .ui-slider.ui-slider-horizontal.ui-widget.ui-widget-content.ui-corner-all {
+    background: #dbdbdb none repeat scroll 0 0;
+    border: medium none;
+    border-radius: 50px;
+    height: 5px;
+    margin-bottom: 12px;
+    margin-left: auto;
+}
+.price_filter .ui-slider-range {
+    background: #0363cd none repeat scroll 0 0;
+}
+.price_filter .ui-slider-handle.ui-state-default.ui-corner-all {
+    background: #0363cd none repeat scroll 0 0;
+    border: 2px solid #fff;
+    border-radius: 100%;
+    height: 15px;
+    width: 15px;
+}
+
+.ui-slider-horizontal .ui-slider-handle {
+    top: -6px;
+}
+.price_filter.mt-40 > span {
+    color: #242424;
+    display: block;
+    margin-bottom: 30px;
+}
+.price_slider_amount .label-input input {
+    background: transparent none repeat scroll 0 0;
+    border: medium none;
+    box-shadow: none;
+    color: #363f4d;
+    font-size: 14px;
+    height: auto;
+    margin: 3px 0 14px;
+    padding-left: 49px;
+    width: 100%;
+}
+.price_slider_amount > button {
+    background-color: #242424;
+    border: medium none;
+    border-radius: 5px;
+    color: #fff;
+    line-height: 1;
+    padding: 8px 16px;
+    cursor: pointer;
+    transition: all .3s ease 0s;
+}
+.price_slider_amount > button:hover {
+    background-color: #047afe;
+}
+.price_filter > span {
+    color: #242424;
+    display: block;
+    margin: 0 0 21px;
+}
+
+</style>
+
+<script>
+import ShopWidget from '~/components/SidebarFilter/ShopWidget';
+
+export default {
+    components: {
+        ShopWidget
+    }
+
+};
+</script>
+
+
