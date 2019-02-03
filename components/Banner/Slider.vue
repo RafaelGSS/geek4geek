ea73d758219902c037550ba22420bfc8d02b484e
Yuri Silva <nogueiranetoy@gmail.com>
2019-02-03 20:37:31 -0200

    ajuste slider

--- components/Banner/Slider.vue
+++ components/Banner/Slider.vue
@@ -3,10 +3,13 @@
     <no-ssr>
       <carousel :items=1 :loop=1 :center=1 :autoplay=1 :autoplayHoverPause=1 :nav=0>
         <a href="#" class="slider-active bg-img">
-            <img src="/img/slider/slider-51.jpg" class="single-slider bg-img slider-height-22">
+            <img id="img1" src="/img/slider/slider-51.jpg" class="single-slider bg-img slider-height-22">
+            <img id="img2" src="/img/slider/single-slider-4.png" class="single-slider bg-img slider-height-22">
+            
         </a>
         <a href="#" class="slider-active bg-img">
-            <img src="/img/slider/slider-51.jpg" class="single-slider bg-img slider-height-22">
+            <img id="img1" src="/img/slider/slider-51.jpg" class="single-slider bg-img slider-height-22">
+            <img id="img2" src="/img/slider/single-slider-4.png" class="single-slider bg-img slider-height-22">
         </a>
       </carousel>
     </no-ssr>
@@ -27,7 +30,13 @@ export default {
 .slider-active.owl-carousel .owl-item .slider-img img {
     width: 100%;
 }
+#img1 {display:block;}
+#img2 {display:none}
 
+@media all and (max-width: 600px) {
+    #img1 {display: none;}
+    #img2 {display: block;}
+}
 </style>
 <style>
 
