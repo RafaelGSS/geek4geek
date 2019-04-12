87c1aa001ab4f6f8e1997b219f132ce9cb8cbbcb
RafaelGSS <rafael.nunu@hotmail.com>
2019-04-12 14:31:50 -0300

    feat: Added NavbarBasic

---
+++ components/navigation/NavbarBasic.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <a href="#"></a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
