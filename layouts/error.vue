739f4eb6fe3cb892eb03977e8ccecab90fb1308a
RafaelGSS <rafael.nunu@hotmail.com>
2019-05-13 20:40:22 -0300

    feat: Added E403 to layouts error

--- layouts/error.vue
+++ layouts/error.vue
@@ -8,20 +8,38 @@
         <div v-if="error.statusCode === 500">
           <E500/>
         </div>
-
+        <div v-if="error.statusCode === 403">
+          <E500/>
+        </div>
         <div class="go-home">
           <button class="btn btn-success">Quero ir para Homepage</button>
         </div>
       </div>
     </div>
-    <div style="background-color: #4a4a4a; height: 5px;">
-
-    </div>
+    <div style="background-color: #4a4a4a; height: 5px;"></div>
   </Page>
 </template>
 
-<style scoped>
+<script>
+import Page from "~/components/Page.vue";
+import E404 from "~/components/error/E404";
+import E500 from "~/components/error/E500";
+import E403 from "~/components/error/E403";
 
+export default {
+  props: ["error"],
+  head: {
+    title: "Página não encontrada"
+  },
+  components: {
+    Page,
+    E404,
+    E500
+  }
+};
+</script>
+
+<style scoped>
 .go-home {
   text-align: center;
 }
@@ -38,22 +56,3 @@
   padding-bottom: 100px;
 }
 </style>
-
-
-<script>
-import Page from "~/components/Page.vue";
-import E404 from "~/components/error/E404";
-import E500 from "~/components/error/E500";
-
-export default {
-  props: ["error"],
-  head: {
-    title: "Página não encontrada"
-  },
-  components: {
-    Page,
-    E404,
-    E500
-  }
-};
-</script>
