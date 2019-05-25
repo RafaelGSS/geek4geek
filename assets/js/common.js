872696477746034a44cb40dc864f61823b2fed7d
RafaelGSS <rafael.nunu@hotmail.com>
2019-05-25 18:49:16 -0300

    update: Removed file unused

--- assets/js/common.js
+++
@@ -1,8 +0,0 @@
-// Set array contains
-Array.prototype.arrayContains = function (sub) {
-    var self = this;
-    var result = sub.filter(function (item) {
-        return self.indexOf(item) > -1;
-    });
-    return sub.length === result.length;
-};
